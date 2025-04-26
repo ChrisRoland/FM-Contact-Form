0b43e8a035fbc9a744a00c46b686ab5699d6b101
ChrisRoland <chrisebuberoland@gmail.com>
2025-04-26 11:49:29 +0100

    progrs

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const result = document.getElementById('result');
+const error = document.getElementById('error');
+const errorMessage = document.getElementById('error-message');
