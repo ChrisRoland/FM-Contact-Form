b97e786e45e41b5a8a8297cbc5f1c39d4b536297
ChrisRoland <chrisebuberoland@gmail.com>
2025-04-28 12:39:55 +0100

    progrs

--- script.js
+++ script.js
@@ -16,15 +16,15 @@ form.addEventListener("submit", function (event) {
 
 
 
-// Simulate an error message
-setTimeout(() => {
-  successMessage.style.display = "none";
-  errorMessage.style.display = "block";
-}, 2000);
-// Simulate a successful form submission
-setTimeout(() => {
-  form.reset();
-  successMessage.style.display = "block";
-  errorMessage.style.display = "none";
-}, 1000);
+// // Simulate an error message
+// setTimeout(() => {
+//   successMessage.style.display = "none";
+//   errorMessage.style.display = "block";
+// }, 2000);
+// // Simulate a successful form submission
+// setTimeout(() => {
+//   form.reset();
+//   successMessage.style.display = "block";
+//   errorMessage.style.display = "none";
+// }, 1000);
 
