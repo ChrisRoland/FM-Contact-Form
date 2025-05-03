c1a8344e88fd2186a468a9aefc021bd17aa51c18
ChrisRoland <chrisebuberoland@gmail.com>
2025-05-03 12:53:52 +0100

    progrs

--- script.js
+++ script.js
@@ -42,40 +42,40 @@
     successMessage.style.display = 'none';
   }, 3000);
 
-  //handle form validation error state
-  const inputs = form.querySelectorAll('input, textarea');
-  inputs.forEach((input) => {
-    input.addEventListener('input', function () {
-      if (this.validity.valid) {
-        this.classList.remove('error');
-        this.nextElementSibling.style.display = 'none';
-      } else {
-        this.classList.add('error');
-        this.nextElementSibling.style.display = 'block';
-      }
-    });
-  });
-  //handle radio button validation
-  const radioButtons = form.querySelectorAll('input[type="radio"]');
-  radioButtons.forEach((radio) => {
-    radio.addEventListener('change', function () {
-      const radioGroup = this.closest('.form-group-radio');
-      const errorMessage = radioGroup.querySelector('small');
-      if (this.checked) {
-        errorMessage.style.display = 'none';
-      }
-    });
-  });
-  //handle checkbox validation
-  const checkbox = form.querySelector('input[type="checkbox"]');
-  checkbox.addEventListener('change', function () {
-    const errorMessage = document.querySelector('.error-state');
-    if (this.checked) {
-      errorMessage.style.display = 'none';
-    } else {
-      errorMessage.style.display = 'block';
-    }
-  });
+//   //handle form validation error state
+//   const inputs = form.querySelectorAll('input, textarea');
+//   inputs.forEach((input) => {
+//     input.addEventListener('input', function () {
+//       if (this.validity.valid) {
+//         this.classList.remove('error');
+//         this.nextElementSibling.style.display = 'none';
+//       } else {
+//         this.classList.add('error');
+//         this.nextElementSibling.style.display = 'block';
+//       }
+//     });
+//   });
+//   //handle radio button validation
+//   const radioButtons = form.querySelectorAll('input[type="radio"]');
+//   radioButtons.forEach((radio) => {
+//     radio.addEventListener('change', function () {
+//       const radioGroup = this.closest('.form-group-radio');
+//       const errorMessage = radioGroup.querySelector('small');
+//       if (this.checked) {
+//         errorMessage.style.display = 'none';
+//       }
+//     });
+//   });
+//   //handle checkbox validation
+//   const checkbox = form.querySelector('input[type="checkbox"]');
+//   checkbox.addEventListener('change', function () {
+//     const errorMessage = document.querySelector('.error-state');
+//     if (this.checked) {
+//       errorMessage.style.display = 'none';
+//     } else {
+//       errorMessage.style.display = 'block';
+//     }
+//   });
   //handle textarea validation
   const textarea = form.querySelector('textarea');
   textarea.addEventListener('input', function () {
