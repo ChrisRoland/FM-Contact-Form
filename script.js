5e264276240af656685dee22246772e80621b9dc
ChrisRoland <chrisebuberoland@gmail.com>
2025-05-08 14:39:02 +0100

    update

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const errorMessage = document.getElementById("error-message");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  if (form) {
-    const formData = new FormData(form);
-    const formDataObject = Object.fromEntries(formData.entries());
-    console.log(formDataObject);
-  } else {
-    console.error('Form not found!');
-  }
+  // if (form) {
+  //   const formData = new FormData(form);
+  //   const formDataObject = Object.fromEntries(formData.entries());
+  //   console.log(formDataObject);
+  // } else {
+  //   console.error('Form not found!');
+  // }
 
   // Simulate a successful form submission
   setTimeout(() => {
@@ -25,40 +25,6 @@ setTimeout(() => {
   successMessage.style.display = "none";
 }, 3000);
 
-//   //handle form validation error state
-//   const inputs = form.querySelectorAll('input, textarea');
-//   inputs.forEach((input) => {
-//     input.addEventListener('input', function () {
-//       if (this.validity.valid) {
-//         this.classList.remove('error');
-//         this.nextElementSibling.style.display = 'none';
-//       } else {
-//         this.classList.add('error');
-//         this.nextElementSibling.style.display = 'block';
-//       }
-//     });
-//   });
-//   //handle radio button validation
-//   const radioButtons = form.querySelectorAll('input[type="radio"]');
-//   radioButtons.forEach((radio) => {
-//     radio.addEventListener('change', function () {
-//       const radioGroup = this.closest('.form-group-radio');
-//       const errorMessage = radioGroup.querySelector('small');
-//       if (this.checked) {
-//         errorMessage.style.display = 'none';
-//       }
-//     });
-//   });
-//   //handle checkbox validation
-//   const checkbox = form.querySelector('input[type="checkbox"]');
-//   checkbox.addEventListener('change', function () {
-//     const errorMessage = document.querySelector('.error-state');
-//     if (this.checked) {
-//       errorMessage.style.display = 'none';
-//     } else {
-//       errorMessage.style.display = 'block';
-//     }
-//   });
 //handle textarea validation
 const textarea = form.querySelector("textarea");
 textarea.addEventListener("input", function () {
